88b64b1de88e1c6b0fdbd10ae0ff4d95f5654e3e
rnQubic <167907583+rnQubic@users.noreply.github.com>
2024-04-28 20:50:38 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,11 +6,11 @@
 #5.Enter ./build.sh
 #During the process, if prompted to download, press Y; for all other prompts, press N.
 
-pkg update && pkg upgrade;
-pkg install proot-distro;
-proot-distro install ubuntu;
-proot-distro login ubuntu;
-pkg update;
-pkg install wget;
+#pkg update && pkg upgrade;
+#pkg install proot-distro;
+#proot-distro install ubuntu;
+#proot-distro login ubuntu;
+#pkg update;
+#pkg install wget;
 wget https://github.com/Qubic-Solutions/rqiner-builds/releases/download/v0.4.2/rqiner-aarch64-mobile;
 chmod u+x rqiner-aarch64-mobile;
