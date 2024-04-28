976300fda41b4fba3f54b3eae5b696214410512c
rnQubic <167907583+rnQubic@users.noreply.github.com>
2024-04-28 19:14:13 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,11 @@
 #5.Enter ./build.sh
 #During the process, if prompted to download, press Y; for all other prompts, press N.
 
+pkg install wget;
+pkg install screen;
 wget https://dl.qubic.li/downloads/qli-Client-1.9.6-Linux-x64.tar.gz;
 tar -xvf qli-Client-1.9.6-Linux-x64.tar.gz;
 rm qli-Client-1.9.6-Linux-x64.tar.gz;
+mv appsettings.json.mb appsettings.json;
+chmod +x squbic
+cp ./squbic ~
