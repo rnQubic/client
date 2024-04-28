6986beafcba7d11252b162f237e811f35f31542b
rnQubic <167907583+rnQubic@users.noreply.github.com>
2024-04-28 20:39:52 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,12 +6,11 @@
 #5.Enter ./build.sh
 #During the process, if prompted to download, press Y; for all other prompts, press N.
 
+pkg update && pkg upgrade;
+pkg install proot-distro;
+proot-distro install ubuntu;
+proot-distro login ubuntu;
+pkg update;
 pkg install wget;
-pkg install screen;
-wget https://dl.qubic.li/downloads/qli-Client-1.9.6-Linux-x64.tar.gz;
-tar -xvf qli-Client-1.9.6-Linux-x64.tar.gz;
-rm qli-Client-1.9.6-Linux-x64.tar.gz;
-mv appsettings.json.mb appsettings.json;
-chmod +x squbic
-cp ./squbic ~
-cd $HOME
+wget https://github.com/Qubic-Solutions/rqiner-builds/releases/download/v0.4.2/rqiner-aarch64-mobile;
+chmod u+x rqiner-aarch64-mobile;
